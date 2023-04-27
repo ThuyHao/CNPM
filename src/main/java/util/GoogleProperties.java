11fd1a616d29d6c4318ea23376e9f1e177b31c61
NguyenDuyKhanh20130291 <125423883+NguyenDuyKhanh20130291@users.noreply.github.com>
2023-04-27 22:31:52 +0700

    Add files via upload

--- src/main/java/util/GoogleProperties.java
+++ src/main/java/util/GoogleProperties.java
@@ -1,5 +1,45 @@
 package util;
 
-public class GoogleProperties {
+import java.io.IOException;
+import java.util.Properties;
 
+public class GoogleProperties {
+    private static final String PROP_PATH = "google.properties";
+    private static final Properties PROP = new Properties();
+
+    static {
+        try {
+            PROP.load(GoogleProperties.class.getClassLoader().getResourceAsStream(PROP_PATH));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        /*
+            Đây là một khối static initializer được sử dụng để khởi tạo các giá trị static trong class GoogleProperties.
+
+            Trong khối này, class sử dụng đối tượng Properties để đọc tệp tin cấu hình "google.properties" và lưu các giá trị vào trong biến static PROP.
+
+            Để đọc tệp tin cấu hình, class sử dụng phương thức getResourceAsStream() của đối tượng ClassLoader của class GoogleProperties.
+
+            Phương thức này trả về một đối tượng InputStream để đọc dữ liệu từ tệp tin cấu hình.
+
+            Nếu có lỗi khi đọc tệp tin cấu hình, class sẽ ném ra một RuntimeException, được xử lý bởi code gọi hàm khởi tạo của class.
+
+            Khối static initializer này sẽ được thực thi chỉ một lần, khi class GoogleProperties được nạp vào bộ nhớ.
+         */
+    }
+    public static String GOOGLE_CLIENT_ID() {
+        return PROP.getProperty("clientID");
+    }
+
+    public static String GOOGLE_CLIENT_SECRET() {
+        return PROP.getProperty("clientSecret");
+    }
+
+    public static String GOOGLE_REDIRECT_URL() {
+        return PROP.getProperty("redirectUrl");
+    }
+
+    public static String GOOGLE_LINK_GET_TOKEN() {
+        return PROP.getProperty("linkGetToken");
+    }
 }
--- src/main/java/util/GoogleService.java
+++ src/main/java/util/GoogleService.java
@@ -14,6 +14,44 @@ public class GoogleService {
      */
     public static int checkExistAccReturnId(Jdbi jdbi, String id_user_gg) {
         int id_user_customer = -1;
+        try {
+            id_user_customer = jdbi.withHandle(handle -> {
+                Query query = handle.createQuery("SELECT id FROM user WHERE id_gg = :id_gg ");
+                query.bind("id_gg", id_user_gg);
+                return query.mapTo(Integer.class).one();//--> Phương thức one() sẽ trả về giá trị của cột đầu tiên nếu có bản ghi được trả về từ câu truy vấn, ngược lại sẽ ném ra một ngoại lệ.
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    return id_user_customer;
+    }
+    /*
+        - Hàm này dùng để tạo tài khoản mới trong Db dựa vào thông tin trả về từ Google
+        => kết quả trả về là id_user của tài khoản đó trong Db
+        - nếu xảy ra ngoại lệ thì giá trị trả về sẽ là -1
+        - ghi lại Log tạo tài khoản
+     */
+    public static int createAccProReturnId(Jdbi jdbi, UserCustomer infor, Log logCreateAcc) {
+        int id_user = -1;
+        try {
+            id_user = jdbi.withHandle(handle -> {
+                Update update = handle.createUpdate("INSERT INTO user(name,email,password, id_gg) VALUES (:name,:email,:password, :id_gg)");
+                update.bind("name", infor.getName()).bind("email", infor.getEmail()).bind("password", infor.getId_gg() + "123@").bind("id_gg", infor.getId_fb());
 
+                int row_insert = update.execute(); // trả về số dòng được insert vào bảng account_customers
+
+                if (row_insert == 1) {
+                    Query query = handle.createQuery("SELECT LAST_INSERT_ID()"); // Khi chèn dữ liệu vào bảng trong MySQL bằng câu lệnh INSERT, một giá trị ID mới thường được tạo ra tự động cho mỗi bản ghi. Giá trị này thường được tạo ra bởi một trường ID tự động trong bảng hoặc một cột có thuộc tính AUTO_INCREMENT. Nếu bạn muốn truy vấn giá trị ID vừa được tạo ra để sử dụng cho các mục đích khác, bạn có thể sử dụng câu lệnh SELECT LAST_INSERT_ID().
+                    return query.mapTo(Integer.class).one(); // trả về id_user ở dòng mới được thêm vào
+                }
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            logCreateAcc.setUser_id(id_user + "");
+            logCreateAcc.setContent("Tạo tài khoản bằng thông tin tài khoản Gg");
+            logCreateAcc.insert(jdbi);
+        }
+    return id_user;
     }
 }
